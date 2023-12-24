af96cf099d6c02d004b94376a5bae3d5f24e3237
Rifqi Izzatur <149316666+rifqiizz@users.noreply.github.com>
2023-12-24 21:30:49 +0700

    Initial commit

---
+++ src/components/provider/index.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Toaster } from "react-hot-toast";
+
+export const Provider = ({ children }) => {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="light">
+        {children}
+        <Toaster />
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
+};
---
+++ src/components/sharedUI/header.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Button, Switch } from "@nextui-org/react";
+import Link from "next/link";
+export const Header = () => {
+  const { theme, setTheme } = useTheme();
+
+  function setThemeMode(value) {
+    if (value) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }
+
+  return (
+    <header className="flex justify-between items-center">
+      <Link href="/">
+        <div className="text-2xl">berlaku.id</div>
+      </Link>
+      <div className="flex gap-4 items-center">
+        <div>About</div>
+        <div>Features</div>
+        <Link href='/login'>Login</Link>
+        <Link href="/register" passHref>
+            <Button>Register</Button>
+        </Link>
+        
+        <Switch size="sm" onChange={(e) => setThemeMode(e.target.checked)} />
+        <Link href='/logout'>Logout</Link>
+      </div>
+    </header>
+  );
+};
