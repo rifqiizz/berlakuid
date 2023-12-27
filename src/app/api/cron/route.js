c3040db73bdaf69e270f9e761549dff74a68b88e
rifqiizz <rifqi.izzoboy@gmail.com>
2023-12-27 23:57:05 +0700

    New API, New Cron table

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,5 +1,26 @@
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import prisma from "@/utils/prisma";
 
-export async function GET(request) {
-  return NextResponse.json({ message: "Im trying running Cron" });
+export async function POST(req) {
+  const { name } = await req.json();
+
+  try {
+    // Create hashed password
+    console.log('Pure: ',name);
+    const hashedName = await bcrypt.hash(name, 10);
+    console.log('Hashed: ',hashedName);
+    // Create user to database
+    const createCron = await prisma.trialcron.create({
+      data: {
+        name: hashedName
+      }
+    });
+    console.log('Isi data: ',createCron);
+
+    return NextResponse.json({ data: createCron, message: "Cron created successfully" }, { status: 201 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ errorMessage: "Something went wrong. Please try again later" }, { status: 500 });
+  }
 }
