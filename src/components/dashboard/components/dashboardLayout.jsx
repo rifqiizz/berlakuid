ecc5ff9a979d976eb9f28497b942e03168f8419b
RizqiDaniya <rizqi.daniya@mediatropy.com>
2023-12-30 11:02:21 +0800

    card dashboard

--- src/components/dashboard/components/dashboardLayout.jsx
+++ src/components/dashboard/components/dashboardLayout.jsx
@@ -4,11 +4,13 @@ import React from "react";
 export const DashboardLayout = ({ children }) => {
   return (
     <main className="h-screen bg-customBlueLight flex flex-col">
-      <div className="max-w-6xl m-auto py-8 space-y-20 w-[95%] md:w-[100%] lg:w-[100%]">
+      <div className="max-w-6xl m-auto py-5 space-y-20 w-[95%] md:w-[100%] lg:w-[100%]">
         <Header />
         
       </div>
-      <div className="box-white-outer"></div>
+      <div className="box-white-outer">
+        <div className="max-w-6xl m-auto py-5"> { children }</div>
+      </div>
     
     </main>
   );
