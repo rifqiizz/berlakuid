1eac271d25131be6936d8f060f6ad485e44bedf6
rifqiizz <rifqi.izzoboy@gmail.com>
2024-01-02 16:38:40 +0700

    Release apiurl

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,2 +1,2 @@
-//export const apiUrl = "https://app.berlaku.id/api";
-export const apiUrl = "http://localhost:3000/api";
+export const apiUrl = "https://app.berlaku.id/api";
+//export const apiUrl = "http://localhost:3000/api";
