c10a3d9af19dbdc74da2c01b8dffaba282e8f432
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-01-03 11:48:39 +0700

    fixing all issue

--- src/app/api/mailer/route.js
+++ src/app/api/mailer/route.js
@@ -1,29 +1,21 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async (req, res) => {
-  const { email } = req.body;
-  const { method } = req;
+export async function POST(req) {
+  const { email } = await req.json();
 
-  if (method === "POST") {
-    if (!email) {
-      return res.status(400).json({ message: "Missing fields" });
-    }
-
-    try {
-      await resend.sendEmail({
-        from: "Mailer <mailer@berlaku.id>",  
-        to: email,
-        subject: "Test Mail",
-        html: "Test mail <strong>content</strong>",
-      });
-      res.status(200).json({ success: true });
-    } catch (error) {
-      res.status(400).json({ success: false, message: error.message });
-    }
-  } else {
-    return res.status(405).json({ message: "Method Not Allowed" });
+  try {
+    await resend.emails.send({
+      from: "Mailer <mailer@berlaku.id>",
+      to: [email],
+      subject: "Test Mail Berlaku",
+      html: "Test mail <strong>content</strong>",
+    });
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ success: false });
   }
-};
+}
