7a04d5c529635444dda51df4c9b1b5413281e95e
Firza <firzaslearning@gmail.com>
2024-01-03 14:14:58 +0700

    category

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,1 +1,4 @@
-export const apiUrl = process.env.NODE_ENV === "production" ? "https://app.berlaku.id/api" : "http://localhost:3000/api";
+export const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://app.berlaku.id/api"
+    : "http://localhost:3000/api";
