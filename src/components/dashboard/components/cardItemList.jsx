25d2130b3c874dca94a805aafcf58febc6f03f1a
daniya222 <daniyarizqi@gmail.com>
2024-01-04 17:17:46 +0800

    change date format

--- src/components/dashboard/components/cardItemList.jsx
+++ src/components/dashboard/components/cardItemList.jsx
@@ -3,6 +3,14 @@ import React from "react";
 
 const CardItemList = ({ id, name, category, reminderOn, username, slug, source }) => {
 
+  
+  const date = new Date(reminderOn);
+
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = date.toLocaleDateString('en-US', options);
+
+  //console.log(formattedDate)
+
   return (
     <div className="card-item-list">
       <Link href={`/${username}/${slug}`}>
@@ -10,7 +18,7 @@ const CardItemList = ({ id, name, category, reminderOn, username, slug, source }
           <div className="hidden">{id}</div>
           <div className="md:basis-1/2 basis-full name-task">{name}</div>
           <div className="md:basis-2/12 basis-full first-letter:cat-task">{category}</div>
-          <div className="md:basis-4/12 basis-full reminder-task">{reminderOn}</div>
+          <div className="md:basis-4/12 basis-full reminder-task">{formattedDate}</div>
         </div>
       </Link>
     </div>
