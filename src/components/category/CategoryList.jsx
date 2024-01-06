b0a140674e9ccbb10d880fafdcfe33d4b8233e09
rifqiizz <rifqi.izzoboy@gmail.com>
2024-01-06 12:17:48 +0700

    prepare icon category

--- src/components/category/CategoryList.jsx
+++ src/components/category/CategoryList.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { PartyPopper, Car, FileText, PanelTop, Package2, Banknote, Home, Computer } from 'lucide-react';
 
-export const CategoryList = ({ id, name, desc }) => {
+export const CategoryList = ({ id, name, desc, icon }) => {
   return (
     <div className="w-full bg-white border rounded-2xl shadow-lg mt-3 p-6 text-center">
+        {icon}
         <h4>{name}</h4>
         <p>{desc}</p>
     </div>
