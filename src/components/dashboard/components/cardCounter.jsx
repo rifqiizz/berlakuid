0a9c7c0c275c64bd1b0076ecaae0497e2c20b932
daniya222 <daniyarizqi@gmail.com>
2024-01-07 12:06:56 +0800

    color card counter

--- src/components/dashboard/components/cardCounter.jsx
+++ src/components/dashboard/components/cardCounter.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const CardCounter = ({ item, text }) => {
-  
+const CardCounter = ({ item, text,bg }) => {
+  const combinedClassName = `card-counter ${bg}`;
   return (
-    <div className="card-counter">
+    <div className={combinedClassName}>
      
       <h1>{item}</h1>
       <h6>{text}</h6>
